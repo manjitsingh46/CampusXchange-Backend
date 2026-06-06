79f39d60d780ece11dcb189b7e14d24211e32c53
manjitsingh46 <manjityadav543@gmail.com>
2026-06-07 05:16:58 +0530

    add swagger ui

--- src/main/java/com/campusxchange/config/SecurityConfig.java
+++ src/main/java/com/campusxchange/config/SecurityConfig.java
@@ -67,6 +67,15 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/api/users/{id}").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/reviews/**").permitAll()
                         .requestMatchers("/actuator/**").permitAll()
+
+                          // Swagger
+                        .requestMatchers(
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/v3/api-docs/**",
+                                "/v3/api-docs",
+                                "/webjars/**"
+                        ).permitAll()
                         .requestMatchers("/ws/**").permitAll()
                         .anyRequest().authenticated()
                 )
